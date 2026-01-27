9e4b3b6a5c45c0feaa576a6d549000df21b7a169
Devron Tombacco <tombacco.devron@gmail.com>
2026-01-27 19:59:44 +0000

    Feat: add all SGs for nginx servers

--- sg.tf
+++ sg.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "sg_nginx" {
+resource "aws_security_group" "sg_nginx1" {
   name        = "sg_nginx"
   description = "Allow SSH + ICMP"
   vpc_id      = aws_vpc.vpc1_ireland.id
@@ -25,6 +25,99 @@ resource "aws_security_group" "sg_nginx" {
   }
 
   tags = {
-    Name = "sg_nginx"
+    Name = "sg_nginx1"
+  }
+}
+
+resource "aws_security_group" "sg_nginx2" {
+  name        = "sg_nginx"
+  description = "Allow SSH + ICMP"
+  vpc_id      = aws_vpc.vpc2_ireland.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip_address}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_nginx2"
+  }
+}
+
+resource "aws_security_group" "sg_nginx4" {
+  name        = "sg_nginx"
+  description = "Allow SSH + ICMP"
+  vpc_id      = aws_vpc.vpc4_london.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip_address}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_nginx4"
+  }
+}
+
+resource "aws_security_group" "sg_nginx5" {
+  name        = "sg_nginx"
+  description = "Allow SSH + ICMP"
+  vpc_id      = aws_vpc.vpc5_london.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip_address}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_nginx5"
   }
 }
