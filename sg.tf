89dcccba6de54d3002b06fb6c46702752e6e4ed5
Devron Tombacco <tombacco.devron@gmail.com>
2026-01-27 19:56:41 +0000

    Feat: add first SG only

---
+++ sg.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "sg_nginx" {
+  name        = "sg_nginx"
+  description = "Allow SSH + ICMP"
+  vpc_id      = aws_vpc.vpc1_ireland.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    #cidr_blocks = ["${var.my_ip_address}/32"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_nginx"
+  }
+}
