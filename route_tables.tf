bc6c9fc41a2911e47a5e2fdb1dc0f8e2497632c7
Devron Tombacco <tombacco.devron@gmail.com>
2026-01-28 13:28:43 +0000

    Fix: add 'provider' attribute to all route tables

--- route_tables.tf
+++ route_tables.tf
@@ -1,5 +1,6 @@
 resource "aws_route_table" "vpc1-rt" {
-  vpc_id = aws_vpc.vpc1_ireland.id
+  vpc_id   = aws_vpc.vpc1_ireland.id
+  provider = aws.ireland
 
   # Route to internet
   route {
@@ -19,7 +20,8 @@ resource "aws_route_table" "vpc1-rt" {
 }
 
 resource "aws_route_table" "vpc2-rt" {
-  vpc_id = aws_vpc.vpc2_ireland.id
+  vpc_id   = aws_vpc.vpc2_ireland.id
+  provider = aws.ireland
 
   #Route to internet
   route {
@@ -40,7 +42,8 @@ resource "aws_route_table" "vpc2-rt" {
 }
 
 resource "aws_route_table" "vpc3-rt" {
-  vpc_id = aws_vpc.vpc3_ireland.id
+  vpc_id   = aws_vpc.vpc3_ireland.id
+  provider = aws.ireland
 
   # Every other internal route
   route {
@@ -56,7 +59,8 @@ resource "aws_route_table" "vpc3-rt" {
 
 
 resource "aws_route_table" "vpc4-rt" {
-  vpc_id = aws_vpc.vpc4_london.id
+  vpc_id   = aws_vpc.vpc4_london.id
+  provider = aws.london
 
   #Route to internet
   route {
@@ -76,7 +80,8 @@ resource "aws_route_table" "vpc4-rt" {
 }
 
 resource "aws_route_table" "vpc5-rt" {
-  vpc_id = aws_vpc.vpc5_london.id
+  vpc_id   = aws_vpc.vpc5_london.id
+  provider = aws.london
 
   #Route to internet
   route {
