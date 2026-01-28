2f3329db7242ccfd178c1af1f34a0233983359ab
Devron Tombacco <tombacco.devron@gmail.com>
2026-01-28 11:41:54 +0000

    Fix: correct typo in provider code in main.tf file

--- igw.tf
+++ igw.tf
@@ -1,5 +1,6 @@
 resource "aws_internet_gateway" "igw1_ireland" {
-  vpc_id = aws_vpc.vpc1_ireland.id
+  provider = aws.ireland
+  vpc_id   = aws_vpc.vpc1_ireland.id
 
   tags = {
     Name = "igw1_ireland"
@@ -7,7 +8,8 @@ resource "aws_internet_gateway" "igw1_ireland" {
 }
 
 resource "aws_internet_gateway" "igw2_ireland" {
-  vpc_id = aws_vpc.vpc2_ireland.id
+  provider = aws.ireland
+  vpc_id   = aws_vpc.vpc2_ireland.id
 
   tags = {
     Name = "igw2_ireland"
@@ -15,15 +17,16 @@ resource "aws_internet_gateway" "igw2_ireland" {
 }
 
 resource "aws_internet_gateway" "igw4_london" {
-  vpc_id = aws_vpc.vpc4_london.id
-
+  vpc_id   = aws_vpc.vpc4_london.id
+  provider = aws.london
   tags = {
     Name = "igw4_london"
   }
 }
 
 resource "aws_internet_gateway" "igw5_london" {
-  vpc_id = aws_vpc.vpc5_london.id
+  vpc_id   = aws_vpc.vpc5_london.id
+  provider = aws.london
 
   tags = {
     Name = "igw5_london"
